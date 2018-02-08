98ccd4464f6a595377301498d1a42738a3aef003
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-02-08 11:58:21 -0300

    initial commit

---
+++ Message.cs
@@ -0,0 +1,52 @@
+﻿namespace NestedNamespaceSwitch
+{
+    namespace Inner
+    {
+        namespace MyNamespace
+        {
+            class MyClassSwitch
+            {
+                public int getNumber(int num)
+                {
+                    int multiplier = 0;
+
+                    switch (num)
+                    {
+                         case 1:
+                             multiplier = 2;
+                             break;
+                         case 2:
+                             multiplier = 5;
+                             break;
+                         case 3:
+                             multiplier = 8;
+                             break;
+                         case 4:
+                         case 5:
+                         case 6:
+                             multiplier = 4;
+                             break;
+                         case 7:
+                             int nm = num % 2; 
+                             switch (nm)
+                             {
+                                 case 0:
+                                     multiplier++;
+                                     break;
+                                 case 2:
+                                     multiplier += 3;
+                                     break;
+                                 default:
+                                     multiplier -= 1;
+                                     break;
+                             }
+                             break;
+         
+                    }
+
+                    return num * multiplier;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestedNamespaceSwitch
+{
+    using Inner.MyNamespace;
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {  
+            Console.WriteLine("Hello World");
+            MyClassSwitch c = new MyClassSwitch();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("getNum {0}:{1}", i, c.getNumber(i));    
+            }
+            
+        }
+    }
+   
+}
